31f5e690309f903a9f3e348e9984d24ea2e7c84c
Seune-Stepanian <114762309+Seune-Stepanian@users.noreply.github.com>
2025-03-06 20:48:52 +0400

    Update CoordinatesTwoThreads.java

--- lesson3_threads/src/main/java/em/eua/distrcomp/lesson3/ex1/CoordinatesTwoThreads.java
+++ lesson3_threads/src/main/java/em/eua/distrcomp/lesson3/ex1/CoordinatesTwoThreads.java
@@ -1,10 +1,8 @@
-package em.eua.distrcomp.lesson3.ex1;
-
 public class CoordinatesTwoThreads {
     public static void main(String[] args) {
-        Walker walker1 = new Walker("person1", new int[]{0, 0});
-        Walker walker2 = new Walker("person2", new int[]{0, 0});
-        new Thread(walker1).start();
-        new Thread(walker2).start();
+        for (int i = 0; i < 10; i++) {
+            Walker walker = new Walker("Person" + i, new int[]{0, 0}, 10);
+            new Thread(walker).start();
+        }
     }
 }
